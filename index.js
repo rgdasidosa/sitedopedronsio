f5992e27ac31a33b58513446d03829c2a6f8ca7d
rgdasidosa <cr7milgrau244@gmail.com>
2024-10-01 16:21:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+//INICIO DA FUNÇÃO
+Fuction Somar(num1, num2){
+    let resultado1;
+    resultado= num1 + num2;
+}
+function Subtrair(num1, num2){
+    let resultado1;
+    resultado= num1-num2;
+
+}
+ function Multiplicar(num1, num2){
+    let resultado;
+    resultado= num1 x num2;
+ }
+
+ function Dividir(num1, num2){
+    let resultado;
+    resultado= num1 ÷ num2;
+ }
+ 
+ //TIPOS DE VARIAVEIS
+
+ //booelan = true or false
+ let matriculado = true;
+ letnaomatriculado = false;
+
+ //Number = numero intei=ro
+ var idade = 1.76;
+
+ //Float = Numeros reais com decimais
+ let altura = 1.76;
+
+ //String = Caracter (letra)
+ const nome = "pedro"
+
+ //Objeto
+ let pessoa = {
+     nome: "João",
+     idade: 16,
+     altura: 1.89
+ }
